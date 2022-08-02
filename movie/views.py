f5072183e8f64500c4f22ab8dfe4c879b9065ade
jpcortesgd <jpcortesg@eafit.edu.co>
2022-08-02 10:00:51 -0500

    Merge with development branch, create about page and html file for this page, and change of color in name in home.html

--- movie/views.py
+++ movie/views.py
@@ -6,3 +6,7 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html', {'name': 'Juan Pablo Cortes'})
+
+
+def about(request):
+    return render(request, 'about.html')
