5de3a80d2a0efb5a104a5514783767b619f68762
jpcortesgd <jpcortesg@eafit.edu.co>
2022-08-02 09:36:45 -0500

    My First Commit

---
+++ moviereviews/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from movie import views as movieViews
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', movieViews.home)
+]
